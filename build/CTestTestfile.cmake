50567b5dbdde033d2c3f8426bf12d95e98cc55fe
dast548 <orlovsasha2007@gmail.com>
2026-06-02 05:19:56 +0000

    Remove build directory from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/abstract_factory
-# Build directory: /home/ubuntu/abstract_factory/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tester "/home/ubuntu/abstract_factory/build/testing")
-set_tests_properties(tester PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/abstract_factory/CMakeLists.txt;23;add_test;/home/ubuntu/abstract_factory/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
